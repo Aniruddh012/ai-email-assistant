e729912d51d21b86a9e0a1550c345a16396e8b7c
Aniruddh012 <aniruddhprajapati213@gmail.com>
2026-05-30 14:09:55 +0530

    add versal link in backend

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,10 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://ai-email-assistant-navy.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 
 app.use(express.json());
